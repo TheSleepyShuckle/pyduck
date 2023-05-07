e3234de0efc9e142fbe67ddc4fa64d96b8cdf9c3
TheSleepyShuckle <nerdsareforever@gmail.com>
2023-05-06 18:07:02 -0600

    Issue #17:  Can pull move types with pokeapi_move_type function

--- types/pokeapi_type_pull_functions.py
+++ types/pokeapi_type_pull_functions.py
@@ -9,7 +9,13 @@ import requests
 def pokeapi_species_types(species_name):
     raw_response = pokeapi_species_call(species_name)
     processed_response = pokeapi_convert_raw_output(raw_response)
-    types = pokeapi_return_types(processed_response)
+    types = pokeapi_return_species_types(processed_response)
+    return types
+
+def pokeapi_move_types(move_name):
+    raw_response = pokeapi_move_call(move_name)
+    processed_response = pokeapi_convert_raw_output(raw_response)
+    types = pokeapi_return_move_type(processed_response)
     return types
 
 # pokeapi_species_call:  takes as input a Pokemon species's name, return pokeapi's
@@ -19,6 +25,14 @@ def pokeapi_species_call(species_name):
     response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{lower_species_name}")
     return response
 
+
+def pokeapi_move_call(move_name):
+    lower_move_name = move_name.lower()
+    lower_dash_move_name = lower_move_name.replace(" ","-")
+    response = requests.get(f"https://pokeapi.co/api/v2/move/{lower_dash_move_name}")
+    return response
+
+
 # pokeapi_convert_raw_output:  takes pokeapi's raw output and converts into a json
 #   file
 def pokeapi_convert_raw_output(json_string):
@@ -26,6 +40,18 @@ def pokeapi_convert_raw_output(json_string):
 
 # pokeapi_return_types:  take processed pokeapi species data and return a list of 
 #   that species's types
-def pokeapi_return_types(species_dict):
+def pokeapi_return_species_types(species_dict):
     types = [type_data["type"]["name"] for type_data in species_dict["types"]]
     return types
+
+def pokeapi_return_move_type(move_dict):
+    # I don't quite understand how the page structure works, but ChatGPT said do this and it works
+    move_type = move_dict['type']['name']
+    move_category = move_dict['damage_class']['name']
+    #include logic for status vs special/physical move
+    if move_category == 'physical':
+        return move_type
+    elif move_category == 'special':
+        return move_type
+    else:
+        return 'na'
